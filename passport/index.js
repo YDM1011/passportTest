bff35417c2a3fd8096443dd7db54d22535700d99
Your Name <technomark1@apnot.com>
2018-04-16 18:51:13 +0300

    add

---
+++ passport/index.js
@@ -0,0 +1,48 @@
+const FacebookStrategy = require('passport-facebook').Strategy;
+const session = require('express-session');
+module.exports = (app , passport) => {
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+    app.use(session({
+        secret: 'keyboard cat',
+        resave: false,
+        saveUninitialized: true,
+        cookie: { secure: false }
+    }));
+
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    passport.use(new FacebookStrategy({
+            clientID: '1644388132319517',
+            clientSecret: '4c5e3bdc2353eebf82cb743aa5e71b59',
+            callbackURL: "http://localhost:3000/auth/facebook/callback"
+        },
+        function(accessToken, refreshToken, profile, done) {
+        console.log(profile);
+            // User.findOrCreate(..., function(err, user) {
+            //     if (err) { return done(err); }
+            //     done(null, user);
+            // });
+            done(null, profile);
+        }
+    ));
+
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', { successRedirect: '/',
+            failureRedirect: '/login' }));
+
+    app.get('/auth/facebook',
+        passport.authenticate('facebook', { scope: 'read_stream' })
+    );
+
+    return passport;
+}
